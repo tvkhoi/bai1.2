1b1b84f04096c1ab49dc69f4de88856cfa52095e
Trần Văn Khởi <tvkhoi123456@gmail.com>
2025-12-25 11:29:51 +0700

    fix

--- saved_script.js
+++ saved_script.js
@@ -1,5 +1,17 @@
-jQuery(document).ready(function($) {
-    $('#dat-hang-header, #dat-hang-main').on('click', function() {
+var link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap";
+document.head.appendChild(link);
+link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap";
+document.head.appendChild(link);
+link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap";
+document.head.appendChild(link);
+jQuery(document).ready(function ($) {
+    $('#dat-hang-header, #dat-hang-main').on('click', function () {
         $('html, body').animate({
             scrollTop: $('#uu-dai').offset().top
         }, 800);
